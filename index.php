16d94dbf99e598f871836135280d3a6d863acf3d
jeroen <jeroenrietveld482@gmail.com>
2016-01-13 15:12:57 +0100

    Removed Node_modules

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
 
 <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.min.js"></script>
-
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
