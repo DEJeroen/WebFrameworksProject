eb7bffcbf79f2c1a051787418367631e037a817e
Boyd220 <boydfranken@msn.com>
2016-01-13 21:12:55 +0100

    detailwifi uitgebreid met straatnaam, huisnr, postcode, gemeente zodat er een lijst met wifi hotspots ontstaat.

--- detailWifi.php
+++ detailWifi.php
@@ -7,8 +7,6 @@
 
 <script src="http://code.jquery.com/jquery-2.1.1.min.js"></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.0.4/angular.min.js"></script>
-
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
@@ -18,49 +16,44 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
-    <script src="app.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
-</head>
 
-<body id="Home" ng-app="myapp">
-
-<section class="Container">
-
-	<div class="content row" ng-controller="mijnCtrl">
-		<?php include "header.php" ?>
-		<div class="col-lg-5">
-			<style>
-      #map2 {
-        width: 300px;
-        height: 250px;
-        background-color: #CCC;
-        margin-left: 2%;
-      }
+    <style>
+    .straat{
+      overflow: auto;
+      border:1px solid blue;
+    }
     </style>
 <script src="https://maps.googleapis.com/maps/api/js"></script>
     <script>
-      function initialize() {
-        var mapCanvas = document.getElementById('map2');
-        var mapOptions = {
-          //vul hier de coordinaten van deze wifi spot in
-          // op deze kaart alleen de huidige wifi locatie laten zien
-          center: new google.maps.LatLng(51.2192159, 4.4028818),
-          zoom: 8,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
+var app = angular.module("myapp",[]);
+
+app.controller("mijnCtrl", function($scope, $http){
+  //console.log("in ctrl");
+
+  $scope.FetchData = function()
+  {
+    $http.get("http://localhost:3000/").success(function(posts){
+      //console.log(lat);
+      $scope.posts = posts;
+    })
+
+    .error(function(err){
+console.log(err);
+    })
+  }
+  $scope.FetchData();
+});
+
     </script>
-<div id="map2"></div>
-    	</div>
-		<div class="col-lg-3">
-<p>Relevante details</p>
-<p>gaan in de vorm van</p>
-<p>id's in deze p tags</p>
-<p>zoals straatnaam en huisnr</p>
-</section>
+</head>
+    <body id="Home" ng-app="myapp" ng-controller="mijnCtrl">  
+
+    <?php include "header.php" ?>
+   
+<div class="straat">
+<li ng-repeat="post in posts">{{post.straat}}, {{post.huisnr}}, postcode: {{post.postcode}}, gemeente: {{post.gemeente}}</li>
 </div>
 
-</body>
+    </body>
 </html>
