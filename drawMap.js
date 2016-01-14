6c59954f93bd2659f95baa1e110cc45201aad7ae
Boyd220 <boydfranken@msn.com>
2016-01-14 14:16:42 +0100

    multiple markers added

--- drawMap.js
+++ drawMap.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var app = angular.module("myapp",[]);
 
 app.controller("mijnCtrl", function($scope, $http){
@@ -63,67 +62,3 @@ google.maps.event.addListener(marker, "click", function(e) {
       }
       google.maps.event.addDomListener(window, 'load', $scope.initialize);
   });
-=======
-var lati = [];
-var longi =[];
-var app = angular.module("myapp",[]);
-
-app.controller("mijnCtrl", function($scope, $http){
- 
-  console.log("in ctrl");
-
-
-
-  $scope.FetchLatitudes = function()
-  {
-    $http.get("http://localhost:3000/latitude").success(function(lat){
-      $scope.lat = lat;
-      lati.push(lat);
-    })
-
-    .error(function(err){
-console.log(err);
-    })
-  }
-
-  $scope.FetchLongitudes = function()
-  {
-    $http.get("http://localhost:3000/")
-    .success(function(lng){
-        $scope.lng = lng;
-        longi.push(lng);
-      })
-
-    .error(function(err){
-console.log(err);
-    });
-
-  }
-
-    $scope.initialise = function() {
-        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
-        var mapOptions = {
-            center: myLatlng,
-            zoom: 16,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
-        navigator.geolocation.getCurrentPosition(function(pos) {
-            map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-            var myLocation = new google.maps.Marker({
-                position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-                map: map,
-                animation: google.maps.Animation.DROP,
-                title: "My Location"
-            });
-        });
-        $scope.map = map;
-    };
-    google.maps.event.addDomListener(document.getElementById("map"), 'load', $scope.initialise());
-
-    $scope.FetchLatitudes();
-    $scope.FetchLongitudes();
-   
-  });
->>>>>>> 46ec582c3d55b292d928c3953a4cce251011a577
