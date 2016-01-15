57af3563c1cc3e97f1cd813a55ba4de25e4d61ea
Boyd220 <boydfranken@msn.com>
2016-01-15 15:36:17 +0100

    Slideshow

--- view/home.php
+++ view/home.php
@@ -1,9 +1,42 @@
 <h2>Show Home</h2>
-
+<div class="col-md-6">
 <div class="jumbotron">
         <p> Welkom op de website gratis wifi-hotspots in Antwerpen. Hier zult
             u een overzicht vinden over alle wifi hotspots die er te vinden zijn
             binnen de stad. </p>
 
+</div>
+</div>
+<div class="col-md-6">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/antwerp-wifi.jpg" alt="wifi">
+    </div>
+    <div class="item">
+      <img src="images/antwerp.jpg" alt="wifi">
+    </div>
+        <div class="item">
+      <img src="images/wifizone.jpg" alt="wifi">
+    </div>
+  </div>
+</div>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div> 
 {{ message }}
