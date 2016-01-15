65f5491c3696bb67ddc0749c2a5d0d01c503f218
jeroen <jeroenrietveld482@gmail.com>
2016-01-15 20:21:01 +0100

    Pretty carousel

--- view/home.php
+++ view/home.php
@@ -1,41 +1,47 @@
-<h2>Welkom op WifispotAntwerpen.be</h2>
-<div class="jumbotron">
-<div class="col-md-4">
+<h2 class="intro-text text-center">Home</h2>
 
+<div class="jumbotron">
         <p> Welkom op de website gratis wifi-hotspots in Antwerpen. Hier zult
             u een overzicht vinden over alle wifi hotspots die er te vinden zijn
             binnen de stad. </p>
-</div>
-<div class="col-md-8">
-    <div id="slideShow" class="carousel slide" data-ride="carousel">
+
+</div> 
+
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
-    <li data-target="#slideShow" data-slide-to="0" class="active"></li>
-    <li data-target="#slideShow" data-slide-to="1"></li>
-    <li data-target="#slideShow" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
   </ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="images/antwerp-wifi.jpg" alt="wifi">
+      <img src="images/antwerp-wifi.jpg" alt="Chania">
     </div>
+
     <div class="item">
-      <img src="images/antwerp.jpg" alt="wifi">
+      <img src="images/antwerp.jpg" alt="Chania">
     </div>
-        <div class="item">
-      <img src="images/wifizone.jpg" alt="wifi">
+
+    <div class="item">
+      <img src="images/wifizone.jpg" alt="Flower">
     </div>
+
   </div>
-</div>
-  <!-- Controls -->
-  <a class="left carousel-control" href="#slideShow" role="button" data-slide="prev">
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="right carousel-control" href="#slideShow" role="button" data-slide="next">
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
     <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-</div> 
 </div>
+
+{{ message }}
